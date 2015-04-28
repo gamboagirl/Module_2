c67f14709248e5760d56f247f2db6cd4e3424dd9
MMG <gamboa_girl@hotmail.com>
2015-04-27 23:08:54 -0700

    Moves functions out of the return.
    Leave the return with just calls to the functions instead of the entire
    function.

--- app/js/enhancedDate.js
+++ app/js/enhancedDate.js
@@ -25,100 +25,107 @@ var enhancedDate = (function () {
     // private data variables
     var myDate;
     
+    function setDate(dateItem) {
+        console.log("setDate");
+        if (dateItem === undefined) {
+            myDate = new Date();
+        } else {
+            myDate = new Date(dateItem);
+        }
+    };
+    
+    function getDate(dateItem) {
+        console.log("getDate");
+        if (dateItem) {
+            return myDate;
+        } else {
+            return Date.UTC(myDate);
+        }
+    };
+        
+    function getDayName() {
+        console.log("getDayName");
+        var day = myDate.getDay();
+        switch (day) {
+            case 0:
+                return "Sunday";
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+                return "Saturday";
+        }
+    };
+        
+    function getMonthName() {
+        console.log("getMonthName");
+        var month = myDate.getMonth();
+        switch (month) {
+            case 0:
+                return "January";
+            case 1:
+                return "February";
+            case 2:
+                return "March";
+            case 3:
+                return "April";
+            case 4:
+                return "May";
+            case 5:
+                return "June";
+            case 6:
+                return "July";
+            case 7:
+                return "August";
+            case 8:
+                return "September";
+            case 9:
+                return "October";
+            case 10:
+                return "November";
+            case 11:
+                return "December";
+        }
+    };
+        
+    function isFuture() {
+        console.log("isFuture");
+        var now = Date.now();
+        if (myDate > now) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+        
+    function isToday() {
+        console.log("isToday");
+        var now = Date.now();
+        if (this.getDate(myDate) == this.getDate(now)) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+    
+    
     return {
         // public methods and properties
         
-        setDate: function(dateItem) {
-            console.log("setDate");
-            if (dateItem === undefined) {
-                myDate = new Date();
-            } else {
-                myDate = new Date(dateItem);
-            }
-        },
-        
-        getDate: function(dateItem) {
-            console.log("getDate");
-            if (dateItem) {
-                return myDate;
-            } else {
-                return Date.UTC(myDate);
-            }
-
-        },
-        
-        getDayName: function() {
-            console.log("getDayName");
-            var day = myDate.getDay();
-            switch (day) {
-                case 0:
-                    return "Sunday";
-                case 1:
-                    return "Monday";
-                case 2:
-                    return "Tuesday";
-                case 3:
-                    return "Wednesday";
-                case 4:
-                    return "Thursday";
-                case 5:
-                    return "Friday";
-                case 6:
-                    return "Saturday";
-            }
-        },
-        
-        getMonthName: function() {
-            console.log("getMonthName");
-            var month = myDate.getMonth();
-            switch (month) {
-                case 0:
-                    return "January";
-                case 1:
-                    return "February";
-                case 2:
-                    return "March";
-                case 3:
-                    return "April";
-                case 4:
-                    return "May";
-                case 5:
-                    return "June";
-                case 6:
-                    return "July";
-                case 7:
-                    return "August";
-                case 8:
-                    return "September";
-                case 9:
-                    return "October";
-                case 10:
-                    return "November";
-                case 11:
-                    return "December";
-            }
-        },
-        
-        isFuture: function() {
-            console.log("isFuture");
-            var now = Date.now();
-            if (myDate > now) {
-                return true;
-            } else {
-                return false;
-            }
-        },
-        
-        isToday: function() {
-            console.log("isToday");
-            var now = Date.now();
-            if (this.getDate(myDate) == this.getDate(now)) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    };
+        setDate: setDate,
+        getDate: getDate,
+        getDayName: getDayName,    
+        getMonthName: getMonthName,
+        isFuture: isFuture,
+        isToday: isToday
+    }
 
 }) ();
 
